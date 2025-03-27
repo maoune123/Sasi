b71bdd0c0caef987db9c0d707247bfe751d48e1b
maoune123 <JUSHWA96@gmail.com>
2025-03-27 13:31:07 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "✅ Bot is running!"
 
 def run():
-    # يتم استخدام منفذ ثابت هنا (على سبيل المثال 10000)
+    # Using a fixed port 10000
     app.run(host="0.0.0.0", port=10000)
 
 def keep_alive():
